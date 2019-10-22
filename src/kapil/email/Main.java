89bf079fb32057e340971e7e72718a2bb9eb31bf
Kapil Krishna <Kapil.Krishna@softwire.com>
2019-10-22 16:16:10 +0100

    after adding sort and stream

--- src/kapil/email/Main.java
+++ src/kapil/email/Main.java
@@ -1,12 +1,14 @@
 package kapil.email;
 
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
 import java.util.regex.*;
-import java.util.HashMap;
+import java.util.stream.Collectors;
+
+import static java.util.Map.*;
 
 
 public class Main {
@@ -21,9 +23,7 @@ public class Main {
         int i = 0;
         Pattern domainNames = Pattern.compile("@\\S*\\w");
         Matcher matcher = domainNames.matcher(content);
-        HashMap<String, Integer> domains = new HashMap<>();
-
-
+        Map<String, Integer> domains = new HashMap<>();
 
         while (matcher.find()) {
             String key = matcher.group();
@@ -31,16 +31,44 @@ public class Main {
             Integer currentValue = domains.getOrDefault(key, 0);
 
             domains.put(key, currentValue + 1);
-
-
         }
-        System.out.println(domains);
 
-//        for (i = 1; i <= content.length() - 14; i++) {
-//            if (content.substring(i, i + 14).equals("@techsmiths.uk")) {
-//                counter++;
-//            }
+        //convert hashmap to entry set
+        Set<Entry<String, Integer>> entries = domains.entrySet();
+
+        //create comparator
+        Comparator<Entry<String, Integer>> compareValues = new Comparator<Entry<String, Integer>>() {
+
+            @Override
+            public int compare(Entry<String, Integer> e1, Entry<String, Integer> e2) {
+                Integer value1 = e1.getValue();
+                Integer value2 = e2.getValue();
+                return value2.compareTo(value1);
+            }
+        };
+
+        //convert set to list
+        List<Entry<String, Integer>> listOfEntries = new ArrayList<Entry<String, Integer>>(entries);
+
+        //sorting values using comparator
+        Collections.sort(listOfEntries, compareValues);
+
+        System.out.println(listOfEntries.subList(0, 10));
+
+        List<Entry<String, Integer>> top10Entries = domains.entrySet()
+                .stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(10)
+                .collect(Collectors.toList());
+
+        System.out.println(top10Entries);
+
+//        LinkedHashMap<String, Integer> sortedDomains = new LinkedHashMap<>(entries.size());
+//
+//        //copy entries from list to map
+//        for (Entry<String, Integer> entry : listOfEntries) {
+//            sortedDomains.put(entry.getKey(), entry.getValue());
 //        }
-//        System.out.println(counter);
+
     }
 }
